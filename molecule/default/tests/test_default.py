d7629116ca829d7f730b470672a11699288c1e65
root <root@gamma2.888toto.bet>
2018-03-23 13:33:10 +0300

    added tests to Tomcat role

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,6 +6,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_executable_file(host):
-    tomcat = host.file('/opt/tomcat/bin')
+def test_executables_folder(host):
+    tomcat = host.file('/opt/tomcat')
     assert tomcat.exists
+    assert tomcat.is_symlink
+
+
+def test_service(host):
+    tomcat = host.service('tomcat')
+    assert tomcat.is_running
+
+
+def test_curl_output(host):
+    assert host.check_output('curl localhost:8080').find(u'Tomcat') > -1
